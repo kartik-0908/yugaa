6741ecab9bb9ec9fb0e148e2b47a69f7a821375a
Kartik <kartik_a@mt.iitr.ac.in>
2024-07-17 03:45:28 +0530

    updates

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
   extends: ["@repo/eslint-config/next.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
+    parser: '@typescript-eslint/parser',
     project: './tsconfig.json',
   },
 };
